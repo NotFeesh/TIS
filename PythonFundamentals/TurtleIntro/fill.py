0e5e869942708f54a90bbddae956de5df0c0e1d7
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-06 19:40:47 -0800

    square lv2 challenge solution and fill.py

---
+++ PythonFundamentals/TurtleIntro/fill.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+t.begin_fill()
+
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+
+t.fillcolor("red")
+# fillcolor can be placed right before the end_fill()
+t.end_fill()
+
+t.mainloop()
---
+++ PythonFundamentals/TurtleIntro/square2_challenge_sol.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+t.color("red")
+t.bgcolor("blue")
+
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+
+t.mainloop()
