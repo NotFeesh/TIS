f99ab6553d4c5ef09fb1082b4604365f348165d2
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-06 19:44:28 -0800

    square 3 challenge solution

---
+++ PythonFundamentals/TurtleIntro/ChallengeSolutions/square3_challenge_sol.py
@@ -0,0 +1,19 @@
+import turtle as t
+
+t.color("red")
+t.bgcolor("blue")
+
+t.begin_fill()
+
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(300)
+
+t.fillcolor("purple")
+t.end_fill()
+
+t.mainloop()
