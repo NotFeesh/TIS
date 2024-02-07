50d037cb64631a154fe285f0f3c7200aea9802cf
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-06 20:04:08 -0800

    shapestamps.py revised, stamps challenge solution

---
+++ PythonFundamentals/TurtleIntro/ChallengeSolutions/stamps_challenge_sol.py
@@ -0,0 +1,27 @@
+import turtle as t
+
+t.stamp()
+t.forward(50)
+
+t.shape("arrow")
+t.stamp()
+t.forward(50)
+
+t.shape("turtle")
+t.stamp()
+t.forward(50)
+
+t.shape("circle")
+t.stamp()
+t.forward(50)
+
+t.shape("square")
+t.stamp()
+t.forward(50)
+
+t.shape("triangle")
+t.stamp()
+
+t.hideturtle()
+
+t.mainloop()
--- PythonFundamentals/TurtleIntro/shapestamps.py
+++ PythonFundamentals/TurtleIntro/shapestamps.py
@@ -1,26 +1,18 @@
 import turtle as t
 
 t.stamp()
-t.forward(50)
 
-t.shape("arrow")
-t.stamp()
-t.forward(50)
+t.hideturtle()
+
+t.forward(100)
 
 t.shape("turtle")
 t.stamp()
-t.forward(50)
 
-t.shape("circle")
-t.stamp()
-t.forward(50)
+t.forward(100)
 
-t.shape("square")
-t.stamp()
-t.forward(50)
+t.forward(100)
 
-t.shape("triangle")
-t.stamp()
-t.forward(50)
+t.showturtle()
 
 t.mainloop()
