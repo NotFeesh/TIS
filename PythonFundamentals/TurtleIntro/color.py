d9f96d0ddf22a565b5df7909c3edb2d040909a6e
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-06 19:36:46 -0800

    finished color.py

---
+++ PythonFundamentals/TurtleIntro/color.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+#intro to color
+t.color("red")
+#teach blue, brown, cyan, gold, gray, green, orange, pink, purple, red, yellow, violet
+#full color list -> https://cs111.wellesley.edu/labs/lab02/colors
+t.forward(300)
+t.right(90)
+t.color("blue")
+t.forward(300)
+
+t.bgcolor("orange")
+
+t.mainloop()
