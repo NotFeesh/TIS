8da3511ae7ef3e6caa7eb7c558a9e93a757f5643
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-06 19:30:08 -0800

    finished movement.py

--- PythonFundamentals/TurtleIntro/movement.py
+++ PythonFundamentals/TurtleIntro/movement.py
@@ -1,7 +1,9 @@
 #t can be replaced with anything, it's just a name
 import turtle as t
 
+#ANY number can go into the parameter, including floats
 t.forward(300)
+#degree measure
 t.right(90)
 t.backward(300)
 t.left(45)
