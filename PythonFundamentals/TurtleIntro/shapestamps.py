59c8e8f7abd85beb55c40a589311ed0d4d006c95
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-06 20:00:08 -0800

    shapestamps.py finished

---
+++ PythonFundamentals/TurtleIntro/shapestamps.py
@@ -0,0 +1,26 @@
+import turtle as t
+
+t.stamp()
+t.forward(50)
+
+t.shape("arrow")
+t.stamp()
+t.forward(50)
+
+t.shape("turtle")
+t.stamp()
+t.forward(50)
+
+t.shape("circle")
+t.stamp()
+t.forward(50)
+
+t.shape("square")
+t.stamp()
+t.forward(50)
+
+t.shape("triangle")
+t.stamp()
+t.forward(50)
+
+t.mainloop()
