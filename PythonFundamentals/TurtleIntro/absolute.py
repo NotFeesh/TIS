caf620755eef50a089d4c0fc8b8e284b31268bdd
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-08 21:05:21 -0800

    absolute.py

---
+++ PythonFundamentals/TurtleIntro/absolute.py
@@ -0,0 +1,20 @@
+import turtle as t
+
+t.forward(400)
+t.goto(0, 0)
+t.backward(400)
+t.goto(400, 400)
+t.goto(0, 0)
+
+t.clearscreen()
+
+t.setheading(90)
+t.forward(300)
+t.setheading(180)
+t.forward(300)
+t.setheading(270)
+t.forward(300)
+t.setheading(0)
+t.forward(300)
+
+t.mainloop()
