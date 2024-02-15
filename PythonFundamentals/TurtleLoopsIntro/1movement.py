b0098a9a8c8e8aa4dde775ce3533acc814152ad0
NotFeesh <106921666+NotFeesh@users.noreply.github.com>
2024-02-14 21:16:27 -0800

    adjusting for new curric.

--- PythonFundamentals/TurtleLoopsIntro/1movement.py
+++ PythonFundamentals/TurtleLoopsIntro/1movement.py
@@ -1,12 +1,14 @@
 #t can be replaced with anything, it's just a name
-import turtle as t
+import turtle
 
 #ANY number can go into the parameter, including floats
-t.forward(300)
+turtle.forward(300)
 #degree measure
-t.right(90)
-t.backward(300)
-t.left(45)
-t.forward(100)
+turtle.right(90)
+turtle.backward(300)
+turtle.left(45)
+turtle.forward(100)
 
-t.mainloop()
+turtle.clearscreen()
+
+turtle.mainloop()
---
+++ PythonFundamentals/TurtleLoopsIntro/2loops.py
@@ -0,0 +1,10 @@
+import turtle
+
+for x in range(8):
+  turtle.forward(100)
+  turtle.back(100)
+
+while True:
+  turtle.forward(1)
+
+turtle.mainloop()
